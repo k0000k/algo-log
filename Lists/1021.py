c8062d666b381a1225a7664c8cadca2a5f13584d
HyeGyeong Kook <84006880+k0000k@users.noreply.github.com>
2023-01-18 00:56:12 +0900

    [BOJ] 1021 요세푸스 문제

---
+++ Lists/1021.py
@@ -0,0 +1,18 @@
+N, K = map(int, input().split())
+
+numbers = [i for i in range(1, N + 1)]
+counter = K - 1
+answer = []
+
+while (True):
+    if not(numbers):
+        break
+    if (counter >= len(numbers)):
+        counter -= len(numbers)
+        continue
+    answer.append(numbers[counter])
+    del numbers[counter]
+    counter += K - 1
+
+answer = str(answer)
+print("<" + answer.strip("[]") + ">")
