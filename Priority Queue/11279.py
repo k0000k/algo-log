0f5a29ddcf2d10c58cf9d3e2d5130270e9106055
HyeGyeong Kook <84006880+k0000k@users.noreply.github.com>
2023-01-23 01:16:45 +0900

    Sol: [BOJ] 11279 최대 힙

---
+++ Priority Queue/11279.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+
+h = []
+cnt = int(input())
+
+for i in range(cnt):
+    num = int(sys.stdin.readline().rstrip())
+
+    if (num == 0):
+        if (len(h) == 0):
+            print(0)
+        else:
+            print(-heapq.heappop(h))
+    else:
+        heapq.heappush(h, -num)
